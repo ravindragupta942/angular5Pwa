b741bdf4eafd999c96aabafd1c75f457b7aae9a9
Aman Kumar <kumaraman7056@hotmail.com>
2017-12-25 01:30:56 +0530

    pwa by ravindra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { MatToolbarModule, MatCardModule,MatButtonModule } from '@angular/material';
+import { ImgCardComponent } from './img-card/img-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
